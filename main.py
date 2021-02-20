e68bd976f1bec950bd3fa5bd587eaccd64c3e288
kristechm <77908725+kristechm@users.noreply.github.com>
2021-02-20 16:01:14 -0500

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-from crpapi import CRP
-crp = CRP('94fa7641a896614ab429750e81691137')
