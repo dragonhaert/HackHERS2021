c60d75f0510e577b36f35b647f6086e230f2e4c8
kristechm <77908725+kristechm@users.noreply.github.com>
2021-02-20 16:11:49 -0500

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-from crpapi import CRP
-crp = CRP('94fa7641a896614ab429750e81691137')
-
-# get a specific legislator by CID
-cand = crp.candidates.get('N00007360')
-print(cand['@attributes']['lastname'])
